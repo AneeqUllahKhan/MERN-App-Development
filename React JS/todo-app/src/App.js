35b03f3ff890b60b265316274df7c13e9169c1be
Aneeq Ullah Khan <aneequllah.khan@outlook.com>
2022-09-29 04:20:30 +0500

    Final

--- React JS/todo-app/src/App.js
+++ React JS/todo-app/src/App.js
@@ -1,68 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import {Input, Button} from './components/input';
 import { useState } from 'react';
+import TextField from '@mui/material/TextField';
 
 function App() {
 
+  const [text, setText] = useState("")
+  const [list, setlist] = useState([])
 
-  let [txt,setTxt] = useState();
-  let [list,setlist] = useState([]);
-   
-  function add(){
-    list.push(txt);
+  let add = () => {
+    setlist([...list, text]);
+    setText("")
     console.log(list)
+  }
+  let deleteAll = () => {
+    setlist([]);
+  }
+
+  let deleteItem = (i) => {
+    list.splice(i, 1);
+    setlist([...list]);
+    console.log(list)
+  }
+
+  let edit = (i) => {
+    let a = prompt("text", list[i])
+    list[i] = a
     setlist([...list]);
   }
 
-  
- const deleteall = (i) => {
-    const updateditems = list.filter((txt) => {
-        return i !== txt.value;
-    });
-
-    setlist(updateditems);
-
-  }
-
- 
-  const onDelete = (txt) => {
-    console.log("I am ondelete of todo", txt);
-
-    setlist(list.filter((e) => {
-      return e !== txt;
-    }));
-    console.log("deleted", list)
-  }
-
-
-
-  
-
 
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <h3>Todo App</h3>
-
-        <input onChange={(e)=>{
-          setTxt(e.target.value);
-          console.log(txt)
-
-        }} />
-        <br />
-        <button onClick={()=>add()}>ADD</button> <br />
-        <button  onClick={() => deleteall(txt.value)}>Delete All</button>
-
-
-       {/* Print The Input */}
-        <ul>{list.map((e,i)=>{
-          return <li key={i}>{e} &nbsp; <button onClick={()=>onDelete(e)}>Delete</button> </li> })}
+    <div className="App-header">
+      <header className='t_head'>
+      <h1>Todo App</h1>
+        <div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" sx={{backgroundColor:"white"}}/>
+          <Input value={text} onChange={(e) => setText(e.target.value)} label="Enter a value" />
+          <Button className="addBtn" onClick={add} label="Add" />
+          <Button onClick={deleteAll} label="Delete All" />
+        </div>
+        {/* <p>{text}</p> */}
+        <ul style={{padding: '0px'}}>
+          {list.map((e, i) => {
+            return <li key={i} className='t_list'>{e}
+            <span  className="t_listBtn">
+            <Button onClick={() => edit(i)} label="Edit" />
+            <Button onClick={() => deleteItem(i)} label="Delete" />
+            </span>
+            </li>
+          })}
         </ul>
-
-  
-      
-            
       </header>
     </div>
   );
